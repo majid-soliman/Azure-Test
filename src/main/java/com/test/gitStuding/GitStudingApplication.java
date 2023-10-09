b82fe79dea78d7f4393a90a28ea6064b4bf0d0e3
majid-soliman <majidsoliman@gmail.com>
2023-10-10 00:49:09 +0400

    New Project

---
+++ src/main/java/com/test/gitStuding/GitStudingApplication.java
@@ -0,0 +1,13 @@
+package com.test.gitStuding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitStudingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitStudingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/gitStuding/GitStudingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.gitStuding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitStudingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
