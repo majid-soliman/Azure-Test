1da7c3fc26aa90eff9bb06deb38ae86c22fcc61d
majid-soliman <majidsoliman@gmail.com>
2023-11-01 02:41:11 +0400

    Test

--- src/main/java/com/test/asure/controller/NamesController.java
+++ src/main/java/com/test/asure/controller/NamesController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("names")
 public class NamesController {
 
+	//Majid Azure Test
 	@GetMapping("getNames")
 	public String getNames() {
 		return "Majid Soliman Azure-Test , Success";
