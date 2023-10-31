8df46e5c65e824c3a51ccdd68a525f3e543048f7
majid-soliman <majidsoliman@gmail.com>
2023-11-01 02:09:03 +0400

    changes

---
+++ src/main/java/com/test/asure/AsureApplication.java
@@ -0,0 +1,13 @@
+package com.test.asure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsureApplication.class, args);
+	}
+
+}
